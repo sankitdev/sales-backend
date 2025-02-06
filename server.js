8977d8f44f8bc1934ec119ff0eee02e7d5bdd3ea
Ankit Singh <ankitsingh635101@gmail.com>
2025-02-06 06:35:25 +0530

    add logging middleware and reorder middleware initialization in server.js

--- server.js
+++ server.js
@@ -6,16 +6,20 @@ import { PORT } from "./config/config.js";
 import { logger } from "./logger.js";
 import router from "./routes/index.js";
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
+
 app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
   })
 );
+app.use(express.json());
+app.use(cookieParser());
+app.use((req, res, next) => {
+  logger.info(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
 app.use("/", router);
-// app.use("/", authUser, product);
 const startServer = async () => {
   await connectDb();
   app.listen(PORT, () => {
